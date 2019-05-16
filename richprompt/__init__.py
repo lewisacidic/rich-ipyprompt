5802fe81aa358ea4c4ad259e19c2ece97686901d
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-16 09:24:39 +0100

    chore(version): bump version to v0.0.1

--- richprompt/__init__.py
+++ richprompt/__init__.py
@@ -16,7 +16,7 @@ from .magic import load_ipython_extension, unload_ipython_extension
 __author__ = "Rich Lewis"
 __copyright__ = "Copyright 2019, Rich Lewis"
 __license__ = "MIT"
-__version__ = '0.0.1.dev0'
+__version__ = '0.0.1'
 __maintainer__ = "Rich Lewis"
 __email__ "opensource@richlew.is"
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name="richprompt",
-        version="0.0.1.dev0",
+        version="0.0.1",
         url="https://github.com/lewisacidic/richprompt",
         author="Rich Lewis",
         author_email="opensource@richlew.is",
